1c6f66db3b7691c6cee4ed5e31a233f7dc2d060a
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2024-03-06 16:25:11 +0200

    add User module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-user.dto';
+import { User } from './interfaces/user.interface';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  async updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ): Promise<User> {
+    return this.userService.update(id, updatePasswordDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<string> {
+    return this.userService.remove(id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,100 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-user.dto';
+import { User } from './interfaces/user.interface';
+import { v4 as uuidv4, validate } from 'uuid';
+
+const validateUuid = (id: string): void => {
+  if (!validate(id)) {
+    throw new BadRequestException(`Id ${id} is not valid UUID`);
+  }
+};
+
+const getUserWithoutPassword = (user: User): User => {
+  const userWithoutPassword = { ...user };
+  delete userWithoutPassword.password;
+
+  return userWithoutPassword;
+};
+
+@Injectable()
+export class UserService {
+  private users: User[] = [];
+
+  create(createUserDto: CreateUserDto) {
+    if (!createUserDto.login || !createUserDto.password) {
+      throw new BadRequestException('Missing field login or password');
+    }
+
+    const user: User = {
+      ...createUserDto,
+      createdAt: new Date().getTime(),
+      id: uuidv4(),
+      updatedAt: new Date().getTime(),
+      version: 0,
+    };
+    this.users.push(user);
+
+    return getUserWithoutPassword(user);
+  }
+
+  findAll(): User[] {
+    return this.users.map((user) => getUserWithoutPassword(user));
+  }
+
+  findOne(id: string): User {
+    validateUuid(id);
+
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+
+    return getUserWithoutPassword(user);
+  }
+
+  update(id: string, updatePasswordDto: UpdatePasswordDto): User {
+    validateUuid(id);
+
+    const userIndex = this.users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+
+    const user = this.users[userIndex];
+
+    if (user.password !== updatePasswordDto.oldPassword) {
+      throw new ForbiddenException(`Old password is incorrect`);
+    }
+
+    this.users[userIndex] = {
+      ...user,
+      version: user.version + 1,
+      updatedAt: new Date().getTime(),
+      password: updatePasswordDto.newPassword,
+    };
+
+    return getUserWithoutPassword(this.users[userIndex]);
+  }
+
+  remove(id: string): string {
+    validateUuid(id);
+
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+
+    this.users = this.users.filter((user) => user.id === id);
+
+    return `User ${id} deleted`;
+  }
+}
