ff4e4da620e587c7b6dea7dfef8f92aeb8d6a6f3
Viktoriia Fedotkina <victoria1f@gmail.com>
2024-03-29 12:17:16 +0200

    add logging to file

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,38 +1,58 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
 import * as fs from 'fs';
-import * as path from 'path';
-
-const dirPath = path.join(__dirname, '../');
+import { LogLevel } from '@nestjs/common/services/logger.service';
 
 @Injectable()
 export class LoggingService extends ConsoleLogger {
-  private logToFile(message: any, context?: string, stack?: string) {
-    const logStream = fs.createWriteStream(`${dirPath}logs.txt`, {
-      flags: 'a',
-    });
-    logStream.write(`${message} ${context} ${super.getTimestamp()} ${stack}\n`);
-    logStream.end();
+  private logStream = fs.createWriteStream(`./src/logs.log`, {
+    encoding: 'utf-8',
+    flags: 'w',
+  });
+  private errorLogStream = fs.createWriteStream(`./src/error-logs.log`, {
+    encoding: 'utf-8',
+    flags: 'w',
+  });
+
+  private logToFile(
+    logLevel: LogLevel,
+    message: any,
+    context?: string,
+    stack?: string,
+  ) {
+    this.logStream.write(
+      `${super.getTimestamp()} [${logLevel.toUpperCase()}] ${message} ${context} ${stack}\n`,
+    );
   }
 
   private logToErrorFile(message: any, stack?: string, context?: string) {
-    const logStream = fs.createWriteStream(`${dirPath}error-logs.txt`, {
-      encoding: 'utf-8',
-      flags: 'a',
-    });
-    logStream.write(
-      `[ERROR] ${message} ${context} ${super.getTimestamp()} ${stack}\n`,
+    this.errorLogStream.write(
+      `${super.getTimestamp()} [ERROR] ${message} ${context} ${stack}\n`,
     );
-    logStream.end();
   }
 
   error(message: any, stack?: string, context?: string) {
-    this.logToFile(message, context, stack);
+    this.logToFile('error', message, context, stack);
     this.logToErrorFile(message, stack, context);
     super.error(message);
   }
 
   log(message: any, context?: string) {
-    this.logToFile(message, context);
+    this.logToFile('log', message, context);
     super.log(message);
+  }
+
+  warn(message: any, context?: string) {
+    this.logToFile('warn', message, context);
+    super.warn(message, context);
+  }
+
+  debug(message: any, context?: string) {
+    this.logToFile('debug', message, context);
+    super.debug(message, context);
+  }
+
+  verbose(message: any, context?: string) {
+    this.logToFile('verbose', message, context);
+    super.verbose(message, context);
   }
 }
